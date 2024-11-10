e05726e3927744a5fc08abfb3204a4c6cb7f611a
PLASMA\clark <gomzclark@gmail.com>
2024-11-10 19:51:41 +0800

    added authentication endpoint

---
+++ KurakuAPI/Program.cs
@@ -0,0 +1,66 @@
+using DataAccess;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+#region OData Model Declarations
+
+var modelBuilder = new ODataConventionModelBuilder();
+modelBuilder.EnableLowerCamelCase();
+
+#endregion
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllers().AddOData(options =>
+    options.EnableQueryFeatures().AddRouteComponents("api", modelBuilder.GetEdmModel()));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("KURAKU_DB"), ServerVersion.Parse("8.0.34-mysql")));
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("api", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+#region Dependency Registrations
+
+
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapIdentityApi<IdentityUser>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
